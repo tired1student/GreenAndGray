4c7fd2889550d45ca32e39a9613a2985b475d63e
tired1student <2782969439@qq.com>
2023-04-25 09:36:57 +0800

    第一次提交，粗略地制作了开始界面

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GameExit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //Application.Quit();
+    }
+}
