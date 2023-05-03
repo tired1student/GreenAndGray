8a89e0ae8eea45f0e436e0b29bc1daf2b957af2e
oraingemuteki <3157590788@qq.com>
2023-05-03 22:06:44 +0800

    Flying Bird项目补丁

--- Assets/Scripts/BirdControll.cs
+++ Assets/Scripts/BirdControll.cs
@@ -6,13 +6,14 @@ using UnityEngine.SceneManagement;
 public class BirdControll : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float speed;
-    public float jumpforce;
-    private bool isDeath = false;
+    
+    private float speed = 1.5f;
+    private float jumpforce = 1;
+    public bool isDeath = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -39,74 +40,20 @@ public class BirdControll : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
         }
     }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Pipe1.1.1")
+        if (collision.gameObject.tag == "Pipe")
+        {
             isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.1.2")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.1.3")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.1.4")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.1.5")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.1.6")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.1.7")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.1")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.2")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.3")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.4")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.5")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.6")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe1.2.7")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.1")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.2")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.3")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.4")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.5")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.6")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.1.7")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.1")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.2")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.3")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.4")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.5")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.6")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe2.2.7")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe3.1")
-            isDeath = true;
-        if (collision.gameObject.tag == "Pipe3.2")
-            isDeath = true;
+        }
     }
+
     void GotoNext()
     {
         if (isDeath)
         {
-            SceneManager.LoadScene("Start");
+            SceneManager.LoadScene("Village");
         }
     }
 }
